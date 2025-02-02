8246afc2365a2fc1a2a8ff9851f12a5861a65f6c
Praveen <113443056+SriArunM@users.noreply.github.com>
2025-02-02 22:39:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ import os
 @st.cache_resource
 def download_model_from_gdrive():
     gdrive_file_id = "13s8_XZ5Q-s1zMyu1BlosawXrvm7EAVQ811"  
-    output_path = "AgeGenderModel.h5"
+    output_path = "AgeGenderModel1.h5"
 
     if not os.path.exists(output_path):
         gdown.download(
